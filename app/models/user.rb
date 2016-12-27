6d855faf61702c46a760c675d3c857e5948d7245
Jimmylee <Jimmylee180180@outlook.com>
2016-12-27 18:51:51 +0800

    use simple_format helper to decorate

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   def is_member_of?(group)
     participated_groups.include?(group)
    end
-end
+
 def join!(group)
     participated_groups << group
   end
@@ -18,3 +18,4 @@ def join!(group)
   def quit!(group)
     participated_groups.delete(group)
   end
+end
